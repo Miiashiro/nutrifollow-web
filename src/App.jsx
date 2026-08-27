e54e9a8579e10005baf12d13f365d8e25063cb64
Miiashiro <miiashiro.caio1@gmail.com>
2026-08-26 21:03:25 -0300

    feat: cria função de login com conexão a api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import Login from "./pages/Login/Login"
+import Home from "./pages/Home/Home"
 
 export default function App(){
   return(
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,32 @@
+import React, { useState } from "react"
 import "./login.css"
 import img from "../../img/logo.png"
 import { Input, PasswordInput, Button } from "@mantine/core"
 import { Lock, User } from "@phosphor-icons/react"
+import api from "../api"
+import { useNavigate } from "react-router"
 
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    async function Login(e){
+        e.preventDefault()
+
+        try{
+            const dataLogin = {email, password}
+
+            const response = await api.post('/login', dataLogin)
+            console.log(response)
+            alert("Login com sucesso")
+            navigate("/Home")
+        } catch(error){
+            console.log(error)
+            alert("Erro")
+        }
+    }
+
     return (
         <div className="login">
             <div className="login-card">
@@ -26,17 +49,21 @@ export default function Login() {
                         placeholder="E-mail"
                         leftSection={<User size={18} />}
                         size="md"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
 
                     <PasswordInput
                         placeholder="Senha"
                         leftSection={<Lock size={18} />}
                         size="md"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
 
                     <p className="forgot">Esqueci minha senha</p>
 
-                    <Button fullWidth size="md" className="enter-btn">Entrar</Button>
+                    <Button fullWidth size="md" className="enter-btn" onClick={Login}>Entrar</Button>
                 </div>
             </div>
         </div>
